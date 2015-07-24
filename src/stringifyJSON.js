df24c9d0f2209b40c547ae268a3b34e23d8dad2d
Laura Knight <ljknight@gmail.com>
2015-07-24 14:51:33 -0700

    stringifyJSON.js - Tests pass for numbers, null, booleans and strings

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,17 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (typeof obj === 'number') {
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (typeof obj === 'string') {
+    // there's gotta be a better way to stringify a string?
+    return '\"' + obj + '\"';
+  }
+
+
 };
