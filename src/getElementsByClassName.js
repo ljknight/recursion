6cf33c54c1eb1a5aa425e1ce42665763b8192d88
Laura Knight <ljknight@gmail.com>
2015-07-27 15:31:11 -0700

    getElementsByClassName.js - Finds direct descendants of body with className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  var bodyChildren = document.body.childNodes;
+
+  // check each element for className (if yes, then push) & check for children >> this is the recursion on each element in the tree
+  
+  for (var i = 0; i < bodyChildren.length; i++) {
+    if (bodyChildren[i].classList && bodyChildren[i].classList.length > 0) {
+      //console.log(bodyChildren[i].classList);
+      if (bodyChildren[i].classList.contains(className)) {
+        //console.log(bodyChildren[i]);
+      }
+    }
+    /*if (bodyChildren[i].classList.contains(className)) {
+      result.push(bodyChildren[i]);
+    }*/
+    /*
+    if (bodyChildren[i].childNodes.length > 0) {
+      console.log(bodyChildren[i].childNodes);
+    }*/
+  }
+
+  //return result;
 };
+
+getElementsByClassName('container-fluid');
