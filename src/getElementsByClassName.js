9733143ddc0c60cfbd4d290a622680046552e983
Laura Knight <ljknight@gmail.com>
2015-07-28 13:38:08 -0700

    getElementsByClassName.js - Passes test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,22 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
 var getElementsByClassName = function(className) {
   var result = [];  
 
   var checkChildren = function(node) {
-    var children = node.childNodes;
+    var children = node.childNodes;  // creates array of node's children
     for (var i = 0; i < children.length; i++) {
-      if (children[i].classList) {
+      if (children[i].classList) {  // checks if element has classes
         if (children[i].classList.contains(className)) {
           result.push(children[i]);
         }
       } 
-    checkChildren(node.childNodes[i]);
+    checkChildren(node.childNodes[i]);  // searches for className on each child element further down the DOM tree
     }
   };
-  checkChildren(document.body);
+  checkChildren(document);
   return result;
 };
-
-//getElementsByClassName('tweet-container');
